fd4d7e455f1a5c85ad6991c8b5753abea8941158
programina-gui <amina.belabbes27@gmail.com>
2020-01-08 15:17:57 +0100

    first successful react code

---
+++ react-test-app/src/components/Todos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Todos extends React.Component {
+
+    render () {
+        return (
+            <div className="App">
+                <h2>Todos - First Component</h2>
+            </div>
+        )
+    }
+
+}
+
+export default Todos;
